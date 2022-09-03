0385ed8e50c99d15fa32d9c09441490e0b317d95
Miyagawa, Toshiki <toshiki.miyagawa@accenture.com>
2022-09-03 20:24:58 +0900

    2022/09/03

---
+++ cmake-build-debug/CMakeFiles/atcoder_workspace.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/toshi/develop/atcoder-workspace/abc113/D/main.cpp" "/mnt/c/Users/toshi/develop/atcoder-workspace/cmake-build-debug/CMakeFiles/atcoder_workspace.dir/abc113/D/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/toshiki/ghq/github.com/atcoder/ac-library"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/atcoder_workspace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atcoder_workspace.dir/abc113/D/main.cpp.o"
+  "atcoder_workspace"
+  "atcoder_workspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/atcoder_workspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
